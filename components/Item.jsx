677f74767ce544b451cfa798f0fef6a1d99a1f13
vietthang332 <thang03032000@gmail.com>
2022-12-06 17:20:16 +0700

    add item component

---
+++ components/Item.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Item({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow mx-auto"
+          width={300}
+          height={400}
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Item;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
+import Item from "../components/Item";
 import Layout from "../components/Layout";
 
 export default function Home() {
+  const sample = {
+    name: "iphone 14pro-max",
+    price: 1500,
+    slug: "mobile-phone",
+    category: "electronic",
+    image:
+      "https://salt.tikicdn.com/cache/w1200/ts/product/de/53/30/2528f727c39e5acb70abf30fd3162978.jpg",
+  };
   return (
     <div>
-      <Layout>
-        <h1>Hello Ecommerce</h1>
+      <Layout title={"Homepage"}>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          <Item product={sample} />
+          <Item product={sample} />
+          <Item product={sample} />
+          <Item product={sample} />
+          <Item product={sample} />
+        </div>
       </Layout>
     </div>
   );
