0d875083e05cea92ba28d3d60296931daf91d05e
vietthang332 <thang03032000@gmail.com>
2022-12-31 04:37:42 +0700

    apply cart context with reducer to handle art to cart action

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import Layout from "../../components/Layout";
+import { Cart } from "../../Context/CartContext";
 import { data } from "../../utils/data";
 
 function ProductScreen() {
+  const { state, dispatch } = useContext(Cart);
   const { query } = useRouter();
   const { id } = query;
   const product = data.find((prod) => prod.id === id);
@@ -14,6 +17,13 @@ function ProductScreen() {
         <h3>Proudct not Found!!!</h3>;
       </Layout>
     );
+  const addToCartHandler = () => {
+    const existItem = state.cartItems.find((item) => item.id === product.id);
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity: existItem ? existItem.quantity + 1 : 1 },
+    });
+  };
   return (
     <Layout>
       <div className="py-2">
@@ -24,8 +34,8 @@ function ProductScreen() {
           <Image
             src={product.image}
             alt={product.name}
-            width={450}
-            height={550}
+            width={640}
+            height={640}
             className="mx-auto"
           />
         </div>
@@ -52,7 +62,12 @@ function ProductScreen() {
               <div>Status</div>
               <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
